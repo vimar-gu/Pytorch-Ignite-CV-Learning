cb5c90742f1047fad8757d9fc22f659f2f3d37fb
ypbehere <583390908@qq.com>
2019-06-04 14:36:13 +0800

    modified datasets

--- data/__init__.py
+++ data/__init__.py
@@ -1,6 +1,16 @@
-from torchvision.datasets import MNIST
+from torchvision.datasets import MNIST, CIFAR10
 from torchvision.transforms import Compose, ToTensor, Normalize
 
-data_transform = Compose([ToTensor(), Normalize((0.1307,), (0.3081,))])
-train_set = MNIST(download=True, root='.', transform=data_transform, train=True)
-test_set = MNIST(download=True, root='.', transform=data_transform, train=False)
+
+def get_dataset(opt):
+	dataset_name = opt.dataset_name
+	dataset_root = opt.dataset_root
+	if dataset_name == 'mnist':
+		data_transform = Compose([ToTensor(), Normalize((0.1307,), (0.3081,))])
+		train_set = MNIST(download=True, root=dataset_root, transform=data_transform, train=True)
+		test_set = MNIST(download=True, root=dataset_root, transform=data_transform, train=False)
+	elif dataset_name == 'cifar':
+		data_transform = Compose([ToTensor(), Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
+		train_set = CIFAR10(download=True, root=dataset_root, transform=data_transform, train=True)
+		test_set = CIFAR10(download=True, root=dataset_root, transform=data_transform, train=False)
+	return (train_set, test_set)
--- train.py
+++ train.py
@@ -1,13 +1,15 @@
 import argparse
 import torch.nn as nn
 from torch.optim import SGD
+from data import get_dataset
 from models import build_model
-from data import train_set, test_set
 from torch.utils.data import DataLoader
 from engine.trainer import do_train
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--model_name', type=str, default='lenet')
+parser.add_argument('--dataset_name', type=str, default='mnist')
+parser.add_argument('--dataset_root', type=str, default='/home/files/dataset/data')
 parser.add_argument('--batch_size', type=int, default=64)
 parser.add_argument('--n_epochs', type=int, default=10)
 parser.add_argument('--lr', type=float, default=0.01)
@@ -16,6 +18,7 @@ parser.add_argument('--log_interval', type=int, default=50)
 opt = parser.parse_args()
 
 model = build_model(opt)
+train_set, test_set = get_dataset(opt)
 train_loader = DataLoader(train_set, batch_size=opt.batch_size, shuffle=True)
 test_loader = DataLoader(test_set, batch_size=opt.batch_size, shuffle=False)
 optimizer = SGD(model.parameters(), lr=opt.lr)
